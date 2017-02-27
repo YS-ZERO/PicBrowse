5ba3564227d6223e9d62125b6651a1bd690570d9
YS <wenjie@wenjiedeMacBook-Pro.local>
2017-02-27 23:52:17 +0800

    修改README.md文件

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PicBrowse
+
+<img src="/example2.png" width="128" height="128" />
+<img src="/example2.png" width="128" height="128" />
+<img src="/app/src/main/res/mipmap-hdpi/ic_launcher.png" width="128" height="128" />
--- app/src/main/java/net/yszero/picbrowse/test/MainActivity.java
+++ app/src/main/java/net/yszero/picbrowse/test/MainActivity.java
@@ -1,15 +1,16 @@
 package net.yszero.picbrowse.test;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import net.yszero.picbrowse.PicPageOptionView;
-import net.yszero.ysutils.base.BaseActivity;
 import net.yszero.ysutils.utils.ImgTypeCast;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends Activity {
 
 
     @Override
@@ -27,13 +28,15 @@ public class MainActivity extends BaseActivity {
         view.setOnPicPageOptionListener(new PicPageOptionView.onPicPageOptionListener() {
             @Override
             public void pageOption(int item) {
-                showShort(item + "");
+                Toast.makeText(MainActivity.this, "第 " + item + " 张图片", Toast.LENGTH_SHORT).show();
             }
         });
 
         int[] ids = {R.drawable.d1, R.drawable.d2, R.drawable.d3, R.drawable.d4, R.drawable.d5,
                 R.drawable.d6, R.drawable.d7, R.drawable.d8, R.drawable.d9, R.drawable.d10,
                 R.drawable.d11, R.drawable.d12, R.drawable.d13, R.drawable.d14, R.drawable.d15};
+
+
         //添加图片
         for (int i = 0; i < ids.length; i++) {
             Drawable d = getResources().getDrawable(ids[i]);
